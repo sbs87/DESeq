1367886d24d681674703dff8d94e55276e9a338c
sbs87 <steve@stevenbsmith.net>
2014-06-20 00:02:01 -0400

    started going under the hood of the diff exp functions to figure out exactly how p values are caclulated for each gene. in the future, go through and undertand each step as it realtes to the paper. comment out. maybe even start a KNITR docgit add *

--- vinette.R
+++ vinette.R
@@ -39,8 +39,56 @@ plotDispEsts( cds )
 head(fitInfo(cds))
 
 res = nbinomTest( cds, "untreated", "treated" )
+nbinomTest
+nbinomTestForMatrices
 head(res)
 head(counts(cds, normalized=TRUE ))
 mean(c(86.06763,62.8034302))
 74.4355310/78.155755
 plotMA(res)
+
+## Under the hood of nbinomTest and nbinomTestForMatrices...
+
+condA="untreated"
+condB="treated"
+colA <- conditions(cds) == condA
+colB <- conditions(cds) == condB
+rawScvA <- fData(cds)[, paste("disp", dispTable(cds)[condA], 
+                              sep = "_")]
+rawScvB <- fData(cds)[, paste("disp", dispTable(cds)[condB], 
+                              sep = "_")]
+
+countsA=counts(cds)[, colA]
+countsB=counts(cds)[, colB]
+sizeFactorsA=sizeFactors(cds)[colA]
+sizeFactorsB=sizeFactors(cds)[colB]
+dispsA=rawScvA
+dispsB=rawScvB
+
+kAs <- rowSums(cbind(countsA))
+kBs <- rowSums(cbind(countsB))
+mus <- rowMeans(cbind(t(t(countsA)/sizeFactorsA), t(t(countsB)/sizeFactorsB)))
+fullVarsA <- pmax(mus * sum(sizeFactorsA) + dispsA * mus^2 *sum(sizeFactorsA^2), mus * sum(sizeFactorsA) * (1 + 1e-08))
+fullVarsB <- pmax(mus * sum(sizeFactorsB) + dispsB * mus^2 *sum(sizeFactorsB^2), mus * sum(sizeFactorsB) * (1 + 1e-08))
+?pmax
+sumDispsA <- (fullVarsA - mus * sum(sizeFactorsA))/(mus *sum(sizeFactorsA))^2
+sumDispsB <- (fullVarsB - mus * sum(sizeFactorsB))/(mus *sum(sizeFactorsB))^2
+sapply(seq(along = kAs), function(i) {
+  if (kAs[i] == 0 & kBs[i] == 0) 
+    return(NA)
+  ks <- 0:(kAs[i] + kBs[i])
+  ps <- dnbinom(ks, mu = mus[i] * sum(sizeFactorsA), size = 1/sumDispsA[i]) * 
+    dnbinom(kAs[i] + kBs[i] - ks, mu = mus[i] * sum(sizeFactorsB), 
+            size = 1/sumDispsB[i])
+  pobs <- dnbinom(kAs[i], mu = mus[i] * sum(sizeFactorsA), 
+                  size = 1/sumDispsA[i]) * dnbinom(kBs[i], mu = mus[i] * 
+                                                     sum(sizeFactorsB), size = 1/sumDispsB[i])
+  stopifnot(pobs == ps[kAs[i] + 1])
+  if (kAs[i] * sum(sizeFactorsB) < kBs[i] * sum(sizeFactorsA)) 
+    numer <- ps[1:(kAs[i] + 1)]
+  else numer <- ps[(kAs[i] + 1):length(ps)]
+  min(1, 2 * sum(numer)/sum(ps))
+})
+
+
+nbinomTestForMatrices(counts(cds)[, colA], counts(cds)[,colB], sizeFactors(cds)[colA], sizeFactors(cds)[colB],rawScvA, rawScvB)

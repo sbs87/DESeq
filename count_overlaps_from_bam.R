156e2456363f2637042358c6ca90f4fcf2aeaeac
sbs87 <steve@stevenbsmith.net>
2014-09-21 23:37:32 -0400

    really polished up counting script to the point that i am using DESeq now. Got it to indpendntly count overlaps and then merge all overlaps once done. i am a genius

--- count_overlaps_from_bam.R
+++ count_overlaps_from_bam.R
@@ -32,11 +32,62 @@ load("TxDb.Hsapiens.ensGene.exbygene")
 alignment_file_list<-as.character(read.table("HR_LA_accepted_hits_list.txt",sep="",header=F)$V1)
 
 BFL<-list() # List to hold all accetped_hits.bam files
+counts<-data.frame()
 for(i in 1:length(alignment_file_list)){
+  print(str(i))
   alignment_file<-alignment_file_list[i] 
-  BFL[[i]]<-BamFile(file = alignment_file) ## Add each file to the BamFileList
+  #BFL[[i]]<-BamFile(file = alignment_file) ## Add each file to the BamFileList
+  #so<-summarizeOverlaps(exbygene, alignment_file, mode="Union")
+  #save(so,file=paste(alignment_file,".overlap",sep=""))
+  load(paste(alignment_file,".overlap",sep=""))
+  if(i == 1){
+    counts<-assays(so)[[1]]
+  }else{
+    counts<-cbind(counts,assays(so)[[1]])
+  }
 }
-reads=BamFileList(BFL) ## Officially create a BamFileList object
-
+#reads=BamFileList(BFL) ## Officially create a BamFileList object
+colnames(counts)<-gsub("_th2out_accepted_hits.bam.sort.bam","",colnames(counts))
 ### And now, the heavy lifting (summarieOverlaps of 56 alignment files)
-sumOl<-summarizeOverlaps(exbygene, reads, mode="Union") ## union is reccomended by DESeq
+#sumOl<-summarizeOverlaps(exbygene, reads, mode="Union") ## union is reccomended by DESeq
+
+library(DESeq2)
+(colData<-data.frame(sample_name=colnames(counts),isoform=c(rep("D",times=6),rep("DL",times=4),rep("HCL",times=6),rep("L",times=6),rep("MED",times=4)),
+      pH=c(rep(4,times=4),rep(7,times=2),rep(4,times=3),7,rep(4,times=4),7,7,4,4,4,4,7,7,rep("MED",times=4)),
+      infection=c(rep("CT",times=2),rep("uninfected",times=4),rep("CT",times=2),rep("uninfected",times=2),rep("CT",times=2),rep("uninfected",times=4),
+                  rep("CT",times=2),rep("uninfected",times=4),rep("CT",times=2),rep("uninfected",times=2)),
+      replicate=1*grepl(pattern = "rep1",colnames(counts))+2*grepl(pattern = "rep2",colnames(counts)))
+)
+dds<-DESeqDataSetFromMatrix(countData=counts,colData=colData,design=~infection + isoform)
+colData(dds)$infection<-factor(colData(dds)$infection,levels=c("uninfected","CT"))
+colData(dds)$isoform<-factor(colData(dds)$isoform,levels=c("MED","HCL","DL","L","D"))
+dds
+dds<-DESeq(dds)
+res<-results(dds)
+(res<-res[order(res$padj),])
+save(res,file="Infection_Isoform.DESeq2Results")
+write.table(rownames(head(res,n=200)),file="Infection_Isoform.topgenes.DESeq2Results",sep="\t")
+plotMA(dds,ylim=c(-2,2))
+
+mcols(res,use.names=TRUE)
+colData(dds)
+resultsNames(dds)
+results(dds,"isoform_vs_MED")
+
+rld<-rlogTransformation(dds,blind=T)
+print(plotPCA(rld,intgroup=c("replicate")))
+plotDispEsts(dds)
+
+library("RColorBrewer")
+library("gplots")
+select<-order(rowMeans(counts(dds,normalized=T)),decreasing = T)[1:30]
+hmcol<-colorRampPalette(brewer.pal(9,"GnBu"))(100)
+heatmap.2(assay(rld)[select,],col=hmcol,
+          Rowv=F,Colv=F,scale="none",
+          dendrogram="none",trace="none",margin=c(10,6))
+
+distsRL<-dist(t(assay(rld)))
+mat<-as.matrix(distsRL)
+rownames(mat)<-colnames(mat)<-with(colData(dds),
+                                   paste(pH,infection,isoform,sep=":"))
+heatmap.2(mat,trace="none",col=rev(hmcol),margin=c(13,13))
